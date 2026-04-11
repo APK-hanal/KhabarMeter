3b7f5c54dd1f459f3bff98017745c3f5245b834a
APK-hanal <apilkhanal10@gmail.com>
2026-04-11 19:44:36 +0545

    Merged two of the scrapers as they did the same thing with the same div names

--- scrape.py
+++ scrape.py
@@ -21,9 +21,10 @@ def get_eco_headerlinks():
             links.append(href)
     #remove duplicate values
     return list(set(links))
+
 #get headers and body content from the specific article
-def eco_scrape(eco_link_list):
-    response = requests.get(eco_link_list,headers=HEAD)
+def scrape(link_list):
+    response = requests.get(link_list,headers=HEAD)
     response.encoding = "utf-8"
     soup = BeautifulSoup(response.text, "html.parser")
     #header
@@ -35,7 +36,7 @@ def eco_scrape(eco_link_list):
     for bodies in body_div.find_all('p'):
         body += bodies.text.strip()
     return {
-        "link" :eco_link_list,
+        "link" :link_list,
         "header" : header,
         "body" : body
     }
@@ -57,33 +58,6 @@ def get_politics_links():
     #remove duplicated values
     return list(set(links))
 
-def politics_scrape(link):
-    response = requests.get(link , headers= HEAD)
-    response.encoding= 'utf-8'
-    soup = BeautifulSoup(response.text , 'html.parser')
-    # headers 
-    header_div = soup.find('div' , class_ = 'ok-post-header')
-    header = header_div.find('h1').text.strip()
-    #body 
-    body = ''
-    body_div = soup.find('div',class_ = 'post-content-wrap')
-    body_paragraphs = body_div.find_all('p')
-    for bod in body_paragraphs:
-        body += bod.text.strip()
-        
-    return {
-        "link":link,
-        'header': header,
-        'body': body
-    }
-    
-    
-    
-    
-    
-    
-
-
 
 # Save to a json file UwU
 def save_article(articles, file):
@@ -97,11 +71,11 @@ if __name__ == "__main__":
     articles_eco = []
     articles_pol = []
     for link in eco_link_list:
-        article = eco_scrape(link)
+        article = scrape(link)
         articles_eco.append(article)
         time.sleep(1)
     for link in politics_links:
-        article = politics_scrape(link)
+        article = scrape(link)
         articles_pol.append(article)
         time.sleep(1)
     save_article(articles_eco,'data/economy.json')
