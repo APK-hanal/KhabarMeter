a3da4e6417a531c756dd0d750b8621de67e6b9c0
APK-hanal <apilkhanal10@gmail.com>
2026-04-12 16:49:26 +0545

    Fixed result bug in app.py

--- app.py
+++ app.py
@@ -12,16 +12,14 @@ def load_results(file):
         return json.load(f)
 
 @app.route('/results/economy')
-def eco_results(file):
-    data = load_results(file)
+def eco_results():
+    data = load_results('data/eco_results.json')
     return jsonify(data)
 
 @app.route('/results/politics')
-def pol_results(file):
-    data = load_results(file)
+def pol_results():
+    data = load_results('data/pol_result.json')
     return jsonify(data)
 
 if __name__ == "__main__":
-    eco_results('data/eco_results.json')
-    pol_results('data/pol_result.json')
     app.run(debug=True)
